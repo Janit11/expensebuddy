e51c16aa3a88c55627cf681457bca4c56c7b4c72
Janit Kumar <kjanit15@gmail.com>
2024-03-24 11:36:48 -0700

    add arrow on activity bar

--- src/components/ActivityBar.js
+++ src/components/ActivityBar.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import '../App.css';
+import { useNavigate } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
 
 const ActivityBar = ({ currentActivity }) => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="activity-bar">
+      <button onClick={goBack} className="back-button">
+        <FaArrowLeft/>
+      </button>
       <span>{currentActivity}</span>
     </div>
   );
